058a26ea29ac3683c18317da556db584e3d0e132
Trevor <trevorhilz@gmail.com>
2024-12-09 16:47:24 -0600

    hashmap for anagram groups

--- neetcode-anagram-groups.js
+++ neetcode-anagram-groups.js
@@ -1,7 +1,14 @@
 const groupAnagrams = (strs) => {
-  const output = [];
-  while (strs.length > 0) {}
-  return output;
+  const map = new Map();
+
+  for (let str of strs) {
+    const sorted = str.split('').sort().join('');
+    if (!map.has(sorted)) {
+      map.set(sorted, []);
+    }
+    map.get(sorted).push(str);
+  }
+  return Array.from(map.values());
 };
 
 console.log(groupAnagrams(['act', 'pots', 'tops', 'cat', 'stop', 'hat']));
