b7c98905ab275b04598839eaa7dfc01e718982fa
Trevor <trevorhilz@gmail.com>
2024-12-09 15:56:11 -0600

    adding anagram group

---
+++ neetcode-anagram-groups.js
@@ -0,0 +1,7 @@
+const groupAnagrams = (strs) => {
+  const output = [];
+  while (strs.length > 0) {}
+  return output;
+};
+
+console.log(groupAnagrams(['act', 'pots', 'tops', 'cat', 'stop', 'hat']));
