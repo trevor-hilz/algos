4f07b719ad500fa1462a2385590af46697157c6f
Trevor <trevorhilz@gmail.com>
2024-12-09 21:41:38 -0600

    top-k-frequent elements

---
+++ neetcode-top-k-frequent-elements.js
@@ -0,0 +1,15 @@
+const topKFrequent = (nums, k) => {
+  const map = new Map();
+
+  for (const num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+
+  return Array.from(map.entries())
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, k)
+    .map(([num]) => num);
+};
+
+console.log(topKFrequent([1, 2, 2, 3, 3, 3], 2));
+console.log(topKFrequent([1, 4, 4, 5, 3, 3], 2));
