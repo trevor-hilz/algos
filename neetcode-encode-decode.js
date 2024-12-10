de24e13d14a32b3283a42e21db0995e668082e20
Trevor <trevorhilz@gmail.com>
2024-12-10 14:48:10 -0600

    completed encode decode

--- neetcode-encode-decode.js
+++ neetcode-encode-decode.js
@@ -1,22 +1,44 @@
 class Solution {
   encode(strs) {
-    let string = '';
+    let res = "";
     for (const str of strs) {
-      string += str + ' ';
+      res += (str.length + '#' + str)
     }
-    return string = string.slice(0, -1);
+    return res;
   }
 
   decode(strs) {
-    console.log(strs);
+    if (strs.length === 0) return [];
 
-    return strs.split(' ');
+    const res = [];
+
+    let i = 0;
+    while(i < strs.length) {
+      let j = i;
+      let num = 0;
+      while (strs[j] !== '#') {
+        num = num * 10 + parseInt(strs[j]);
+        j += 1;
+      }
+      j += 1;
+      let k = j
+      let temp = "";
+      while (num > 0) {
+        temp += strs[k];
+        k += 1;
+        num -= 1;
+      }
+      i = k;
+      res.push(temp);
+    }
+    return res;
   }
 }
 
 const test = new Solution();
 console.log(test.encode(['neet', 'code', 'love', 'you']));
-console.log(test.encode([""]));
-const string = test.encode(['neet', 'code', 'love', 'you']);
-// const string = test.encode(['']);
+console.log(test.encode(["we","say",":","yes","!@#$%^&*()"]));
+// const string = test.encode(['neet', 'code', 'love', 'you']);
+const string = test.encode(["we","say",":","yes","!@#$%^&*()"]);
 console.log(test.decode(string));
+
