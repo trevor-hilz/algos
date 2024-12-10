ef2541f77ca0f1dae4362092004e08465c16ecbd
Trevor <trevorhilz@gmail.com>
2024-12-10 15:37:43 -0600

    O(N) for products of array except self

--- neetcode-products-of-array-except-self.js
+++ neetcode-products-of-array-except-self.js
@@ -4,17 +4,34 @@ class Solution {
    * @return {number[]}
    */
   productExceptSelf(nums) {
-    const res = new Array(nums.length).fill(1);
+    // const res = new Array(nums.length).fill(1);
 
-    let temp = 1;
+    // let temp = 1;
 
-    for (let i = 0; i < nums.length; i++) {
-      temp = 1;
-      for (let j = 0; j < nums.length; j++) {
-        if (j === i) continue;
-        temp *= nums[j];
-      }
-      res[i] = temp;
+    // for (let i = 0; i < nums.length; i++) {
+    //   temp = 1;
+    //   for (let j = 0; j < nums.length; j++) {
+    //     if (j === i) continue;
+    //     temp *= nums[j];
+    //   }
+    //   res[i] = temp;
+    // }
+    // return res;
+
+    const n = nums.length; 
+    const res = new Array(n).fill(1);
+
+    let prefix = 1;
+    for(let i = 0; i < n; i++) {
+      res[i] = prefix;
+      prefix *= nums[i];
+    }
+
+    let suffix = 1;
+    for (let i = n - 1; i >= 0; i--) {
+      console.log(res[i], suffix)
+      res[i] *= suffix;
+      suffix *= nums[i];
     }
     return res;
   }
