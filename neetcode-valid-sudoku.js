bb2bd347a68e67217f9527144e357c18b5157c20
Trevor <trevorhilz@gmail.com>
2024-12-12 10:53:40 -0600

    completed valid sudoku

--- neetcode-valid-sudoku.js
+++ neetcode-valid-sudoku.js
@@ -4,20 +4,27 @@ class Solution {
    * @return {boolean}
    */
   isValidSudoku(board) {
-    // create set for rows, cols, and squares
-    // nested for loop to iterate through the sudoku board
-    // checked if board at x/y exists in the row at i set, col at i set, and square at i, y set
-    // if duplicate, return false
-    // at conclusion, return true
-    for(let i = 0; i < board.length; i++) {
-      const set = new Set();
-      for(const data of board[i]) {
-        if(set.has(data) && data !== '.') return false;
-        set.add(data);
+    const rows = Array.from({ length: 9 }, () => new Set());
+    const cols = Array.from({ length: 9 }, () => new Set());
+    const boards = Array.from({ length: 3 }, () =>
+      Array.from({ length: 3 }, () => new Set())
+    );
+
+    for (let r = 0; r < board.length; r++) {
+      for (let c = 0; c < board.length; c++) {
+        if(board[r][c] === '.') continue;
+        if (rows[r].has(board[r][c]) || cols[c].has(board[r][c]) || boards[Math.floor(r / 3)][Math.floor(c / 3)].has(board[r][c])) {
+          return false;
+        } else {
+          if (board[r][c] !== '.') {
+            rows[r].add(board[r][c]);
+            cols[c].add(board[r][c]);
+            boards[Math.floor(r / 3)][Math.floor(c / 3)].add(board[r][c]);
+          }
+        }
       }
-      console.log(set);
     }
-    return 'sudoku';
+    return true;
   }
 }
 
@@ -28,6 +35,20 @@ console.log(
     ['1', '2', '.', '.', '3', '.', '.', '.', '.'],
     ['4', '.', '.', '5', '.', '.', '.', '.', '.'],
     ['.', '9', '8', '.', '.', '.', '.', '.', '3'],
+    ['5', '.', '.', '.', '6', '.', '.', '.', '4'],
+    ['.', '.', '.', '8', '.', '3', '.', '.', '5'],
+    ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+    ['.', '.', '.', '.', '.', '.', '2', '.', '.'],
+    ['.', '.', '.', '4', '1', '9', '.', '.', '8'],
+    ['.', '.', '.', '.', '8', '.', '.', '7', '9'],
+  ])
+);
+
+console.log(
+  test.isValidSudoku([
+    ['1', '2', '.', '.', '3', '.', '.', '.', '.'],
+    ['4', '.', '.', '5', '.', '.', '.', '.', '.'],
+    ['.', '9', '1', '.', '.', '.', '.', '.', '3'],
     ['5', '.', '.', '.', '6', '.', '.', '.', '4'],
     ['.', '.', '.', '8', '.', '3', '.', '.', '5'],
     ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
