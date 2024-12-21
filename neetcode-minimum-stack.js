91a1831192aee64e68f170523896ef02ea4dc797
Trevor <trevorhilz@gmail.com>
2024-12-20 20:35:45 -0600

    minimum stack empty

---
+++ neetcode-minimum-stack.js
@@ -0,0 +1,24 @@
+class MinStack {
+  constructor() {}
+
+  /**
+   * @param {number} val
+   * @return {void}
+   */
+  push(val) {}
+
+  /**
+   * @return {void}
+   */
+  pop() {}
+
+  /**
+   * @return {number}
+   */
+  top() {}
+
+  /**
+   * @return {number}
+   */
+  getMin() {}
+}
