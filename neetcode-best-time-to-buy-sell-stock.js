4bdc808d006d9e6c7a6a8dcc642e355f0a9b5fa8
Trevor <trevorhilz@gmail.com>
2024-12-27 23:17:52 -0600

    better solution to buy sell stock

--- neetcode-best-time-to-buy-sell-stock.js
+++ neetcode-best-time-to-buy-sell-stock.js
@@ -4,13 +4,21 @@ class Solution {
      * @return {number}
      */
     maxProfit(prices) {
-        let highest = 0;
-        for(let i = 0; i < prices.length - 1; i++) {
-            for(let j  = i + 1; j < prices.length; j++) {
-                highest = Math.max(highest, prices[j] - prices[i])
-            }
+        // let highest = 0;
+        // for(let i = 0; i < prices.length - 1; i++) {
+        //     for(let j  = i + 1; j < prices.length; j++) {
+        //         highest = Math.max(highest, prices[j] - prices[i])
+        //     }
+        // }
+        // return highest;
+        let maxP = 0;
+        let minBuy = prices[0];
+
+        for(const sell of prices) {
+            maxP = Math.max(maxP, sell - minBuy);
+            minBuy = Math.min(minBuy, sell);
         }
-        return highest;
+        return maxP;
     }
 }
 
