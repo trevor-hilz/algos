ec09fd5fe3ed13b244c5f9aff69bca40da34c2f5
Trevor <trevorhilz@gmail.com>
2025-01-12 16:44:54 -0600

    added fleet but not done

---
+++ neetcode-car-fleet.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        console.log(target, position, speed)
+        let fleet = 0;
+        let max = position.length * target;
+        while(position);
+            position.map(speed);
+        }
+    return fleet;
+}
+
+const t = new Solution();
+console.log(t.carFleet(10, [1, 4], [2, 1]));
