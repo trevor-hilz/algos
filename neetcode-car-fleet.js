1d6baadb336ece27eb023d9a92cbe3217e09719d
Trevor <trevorhilz@gmail.com>
2025-01-17 21:37:27 -0600

    car fleet algo

--- neetcode-car-fleet.js
+++ neetcode-car-fleet.js
@@ -1,18 +1,25 @@
 class Solution {
-    /**
-     * @param {number} target
-     * @param {number[]} position
-     * @param {number[]} speed
-     * @return {number}
-     */
-    carFleet(target, position, speed) {
-        console.log(target, position, speed)
-        let fleet = 0;
-        let max = position.length * target;
-        while(position);
-            position.map(speed);
-        }
-    return fleet;
+  /**
+   * @param {number} target
+   * @param {number[]} position
+   * @param {number[]} speed
+   * @return {number}
+   */
+  carFleet(target, position, speed) {
+    let pair = position.map((p, i) => [p, speed[i]]);
+    pair.sort((a, b) => b[0] - a[0]);
+    let stack = [];
+    for (let [p, s] of pair) {
+      stack.push((target - p) / s);
+      if (
+        stack.length >= 2 &&
+        stack[stack.length - 1] <= stack[stack.length - 2]
+      ) {
+        stack.pop();
+      }
+    }
+    return stack.length;
+  }
 }
 
 const t = new Solution();
