5e115c77e1793ec5a8f30d34cc6410b3331b20be
Trevor <trevorhilz@gmail.com>
2025-01-17 21:52:12 -0600

    completed largest rectangle

--- neetcode-largest-rectangle-histogram.js
+++ neetcode-largest-rectangle-histogram.js
@@ -4,18 +4,22 @@ class Solution {
    * @return {number}
    */
   largestRectangleArea(heights) {
-      let res = 0;
+    let res = 0;
 
-      for(let i = 0; i < heights.length - 1; i++) {
-          let maxH = heights[i];
-          for (let j = i; j < heights.length; j++) {
-              maxH = Math.min(maxH, heights[j])
-              console.log(j, maxH);
-              let num = 1 + (j - i);
-              let area = maxH * num;
-              res = Math.max(res, area);
-          }
+    for (let i = 0; i < heights.length; i++) {
+      let maxH = heights[i];
+      for (let j = i; j < heights.length; j++) {
+        maxH = Math.min(maxH, heights[j]);
+        console.log(j, maxH, res);
+        let num = Math.max(1, j - i);
+        let area = maxH * num;
+        res = Math.max(res, area);
       }
-      return res;
+    }
+    return res;
   }
 }
+
+const t = new Solution();
+
+console.log(t.largestRectangleArea([1, 3, 7]));
